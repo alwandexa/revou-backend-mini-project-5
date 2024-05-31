4e5edbc0be94e42a2fad25f654f66a8b71bb85d2
alwandexa <alwan.wirawan@djembatandua.com>
2024-05-31 08:52:19 +0700

    feat(modules): Run Routes

--- inventory-service/src/server.ts
+++ inventory-service/src/server.ts
@@ -1,4 +1,12 @@
+import app from "./app";
+import { appConfig } from "./config/config";
 import { InventoryService } from "./services/inventory-service";
 
+const PORT = appConfig.APP_PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`);
+});
+
 InventoryService.checkInventoryRabbit();
-InventoryService.checkInventoryKafka();
+// InventoryService.checkInventoryKafka();
